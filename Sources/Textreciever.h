0af3be534421bbb1b9ce52af6c27e7d4ca418772
mauro <mauronav006@gmail.com>
2021-05-04 12:07:31 -0600

    TextSplitter
    Add classes to manage the text recieved

---
+++ Sources/Textreciever.h
@@ -0,0 +1,14 @@
+//
+// Created by mauro on 5/4/21.
+//
+
+#ifndef C_TEXT_EDITOR_TEXTRECIEVER_H
+#define C_TEXT_EDITOR_TEXTRECIEVER_H
+
+
+class Textreciever {
+
+};
+
+
+#endif //C_TEXT_EDITOR_TEXTRECIEVER_H
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -9,12 +9,14 @@ int main(int argc, char *argv[])
     /*QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();*/
+    return a.exec();
 
     memoryManager *m = new memoryManager;
     int usage = m->CurrentUsage();
 
-    std::cout << "Memory Usage: " << usage << " bytes\n";
+    std::cout << "Memory Usage: " << usage << " bytes\n"; */
+
+
 
 
 }
